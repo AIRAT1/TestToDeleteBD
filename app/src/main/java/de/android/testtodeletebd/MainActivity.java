b66f41e255d6ca4f193de448f40c7e85db7d5c8f
Ayrat <ayrat1@mail.ru>
2016-09-05 22:09:24 +0300

    some refactoring

--- app/src/main/java/de/android/testtodeletebd/MainActivity.java
+++ app/src/main/java/de/android/testtodeletebd/MainActivity.java
@@ -2,10 +2,12 @@ package de.android.testtodeletebd;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,12 +24,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         btnAdd = (Button)findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(this);
-
         btnRead = (Button)findViewById(R.id.btnRead);
-        btnRead.setOnClickListener(this);
-
         btnClear = (Button)findViewById(R.id.btnClear);
+        etName = (EditText)findViewById(R.id.etName);
+        etEmail = (EditText)findViewById(R.id.etEmail);
+
+        btnAdd.setOnClickListener(this);
+        btnRead.setOnClickListener(this);
         btnClear.setOnClickListener(this);
 
         dbHelper = new DBHelper(this);
@@ -36,11 +39,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         ContentValues cv = new ContentValues();
-
         String name = etName.getText().toString();
         String email = etEmail.getText().toString();
-    }
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
 
+        switch (view.getId()) {
+            case R.id.btnAdd:
+                Log.d(TAG, "--- Insert in DB ---");
+                cv.put("name", name);
+                cv.put("email", email);
+                long rowID = db.insert("mytable", null, cv);
+                Log.d(TAG, "row inserted " + rowID);
+
+                etName.setText("");
+                etName.requestFocus();
+                etEmail.setText("");
+                break;
+            case R.id.btnRead:
+                Log.d(TAG, "--- Rows in my table ---");
+                Cursor c = db.query("mytable", null, null, null, null, null, null);
+                if (c.moveToFirst()) {
+                    int idColIndex = c.getColumnIndex("id");
+                    int nameColIndex = c.getColumnIndex("name");
+                    int emailColIndex = c.getColumnIndex("email");
+                    do {
+                        Log.d(TAG, "ID = " + c.getInt(idColIndex)
+                        + ", name " + c.getString(nameColIndex)
+                        + ", email " + c.getString(emailColIndex));
+                    }while (c.moveToNext());
+                }else {
+                    Log.d(TAG, "0 rows");
+                }
+                c.close();
+                break;
+            case R.id.btnClear:
+                Log.d(TAG, "--- Clear myTable ---");
+                int clearCount = db.delete("mytable", null, null);
+                Log.d(TAG, "deleted rows count = " + clearCount);
+                break;
+        }
+        db.close();
+    }
     private class DBHelper extends SQLiteOpenHelper {
 
         public DBHelper(Context context) {
@@ -48,13 +87,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         @Override
-        public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE mytable ("
+            + "id integer primary key autoincrement, "
+            + "name text, "
+            + "email text"
+            + ");");
         }
 
         @Override
-        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+            db.execSQL("DROP TABLE IF EXISTS " + "mytable");
+            onCreate(db);
         }
     }
 }
